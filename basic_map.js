bc91de0b28f7fec297bfa9b8df80d710ad6c0d36
Dylan Lewis <dylanlewis89@gmail.com>
2016-05-05 21:27:04 -0400

    Basic map creation

---
+++ basic_map.js
@@ -0,0 +1,8 @@
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiZHlsYW5sZXdpczg5IiwiYSI6ImNpbnYxN290dDEyc3d1Nmx5NXd5NHhqYXMifQ.cOH98d9HW4pAC2jaaTV42g';
+var map = new mapboxgl.Map({
+    container: 'map', // container id
+    style: 'mapbox://styles/mapbox/streets-v8', //stylesheet location
+    center: [-74.50, 40], // starting position
+    zoom: 9 // starting zoom
+});
