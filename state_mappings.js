93dcbf2de6511ca755dc6aa0fb4cec5c81b04144
Dylan Lewis <dylanlewis89@gmail.com>
2016-05-06 23:18:19 -0400

    Create separate layers for states with tribal names

---
+++ state_mappings.js
@@ -0,0 +1,55 @@
+// taken from http://www.native-languages.org/state-names.htm
+exports.mappings = function() {
+  return {
+    "Alabama": "Choctaw", // or Alabama / Alibamu
+    "Alaska": "Aleut" ,
+    "Arizona": "Tohono O'odham",
+    "Arkansas": "Quapaw",
+    // "California":
+    //"Colorado"
+    "Connecticut": "Mohegan",
+    // "Delaware"
+    // "Florida"
+    // "Georgia"
+    // "Hawaii"
+    // "Idaho"
+    "Illinois": "Illini",
+    //"Indiana"
+    "Iowa": "Ioway",
+    "Kansas": "Kansa",
+    "Kentucky": "Iroquois",
+    //"Louisiana"
+    //"Maine"
+    //"Maryland"
+    "Massachusetts": "Wampanoag",
+    "Michigan": "Potawatomi", // and Ojibwe
+    "Minnesota": "Dakota Sioux",
+    "Mississippi": "Ojibwe",
+    "Missouri": "Missouria",
+    //"Montana"
+    "Nebraska": "Nithaska", // or Nibratha in Omaha-Ponca and Otoe languages
+    //"Nevada"
+    //"New Hampshire"
+    //"New Jersey"
+    "New Mexico": "Aztec",
+    //"New York": "Dakota Sioux",
+    //"North Carolina"
+    "North Dakota": "Dakota Sioux",
+    "Ohio": "Seneca",
+    "Oklahoma": "Choctaw",
+    "Oregon": "Algonquin",
+    //"Pennsylvania"
+    //"Rhode Island"
+    //"South Carolina"
+    "South Dakota": "Dakota Sioux",
+    "Tennessee": "Cherokee",
+    "Texas": "Caddo",
+    "Utah": "Ute",
+    //"Vermont"
+    //"Virginia"
+    //"Washington"
+    //"West Virginia"
+    "Wisconsin": "Ojibwe",
+    "Wyoming": "Lenape"
+  };
+};
